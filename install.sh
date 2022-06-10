48fb947bf60c36ca1c6772f28af2e5b7756db3a9
Aidan Smith <adsmith2@andrew.cmu.edu>
2022-06-10 02:10:22 -0700

    Moved files and fixed install script

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-ln -s .vimrc ~/.vimrc
+DOTFILE_HOME=$(cd "$(dirname "$0")" ; pwd -P)
+
+source "$DOTFILE_HOME/.zprofile"
+
+mkdir "$HOME/.local/bin"
+ln -s "$DOTFILE_HOME/bin/"* "$HOME/.local/bin"
+
+ln -s "$DOTFILE_HOME/.zprofile" "$HOME/.zprofile"
+ln -s "$DOTFILE_HOME/.zshrc" "$ZDOTDIR/.zshrc"
+ln -s "$DOTFILE_HOME/nvim/init.lua" "$XDG_CONFIG_HOME/nvim/init.lua"
+ln -s "$DOTFILE_HOME/nvim/lua" "$XDG_CONFIG_HOME/nvim/lua"
